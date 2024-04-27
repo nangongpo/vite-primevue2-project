234716f3035a907585749f33be7835ddf890cbbf
nangongpo <1844141227@qq.com>
2024-04-27 18:51:20 +0800

    feat: 构建gh-pages

---
+++ dist/js/ProductService-Cwx0hge7.js
@@ -0,0 +1,2 @@
+import{d as a}from"./app.component-DQmWytpG.js";class r{getProductsSmall(){return a.get("/demo/data/products-small.json").then(t=>t.data.data)}getProducts(){return a.get("/demo/data/products.json").then(t=>t.data.data)}getProductsWithOrdersSmall(){return a.get("/demo/data/products-orders-small.json").then(t=>t.data.data)}}export{r as P};
+//# sourceMappingURL=ProductService-Cwx0hge7.js.map
---
+++ dist/js/vite-plugin-fake-server-4rIte7rE.js
@@ -0,0 +1,2 @@
+function n(e){return e}export{n as d};
+//# sourceMappingURL=vite-plugin-fake-server-4rIte7rE.js.map
